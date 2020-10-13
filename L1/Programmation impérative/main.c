f786cb1358528d5a131b895889b147ca8f69caab
Elie Mendy <emendy.stud@gmail.com>
2020-10-13 08:03:45 +0200

    première modif d'un fichier pour constater la remontée dans git hub via atom

---
+++ L1/Programmation impérative/main.c
@@ -0,0 +1,11 @@
+/* un programme de test pour constater les modifs sur github */
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[]) {
+
+  printf("Bonjour\n");
+
+  return 0;
+}
