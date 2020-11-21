2a1806721af65ea993a3c22eb06a23da710fd3c3
Elie Mendy <emendy.stud@gmail.com>
2020-11-21 10:19:34 +0100

    prépa recupération travail en python L1

---
+++ L1/Programmation impérative/Cours/5 - Index Lexicographique/realisation/usage.c
@@ -0,0 +1,10 @@
+#ifndef USAGE
+#define USAGE
+
+void usage(char * message){
+  printf("usage : %s\n",  message);      // message d'erreur
+  exit(1);                              // code erreur
+}
+
+
+#endif
