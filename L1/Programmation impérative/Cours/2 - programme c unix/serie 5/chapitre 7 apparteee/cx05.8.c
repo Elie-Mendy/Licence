08a7bca41bebde2f150f5718ac5d48af0f827c71
Elie <emendy.stud@gmail.com>
2021-04-02 21:03:32 +0200

    LI-39: Lecture et préparation a l'exercice 17.7

--- L1/Programmation impérative/Cours/2 - programme c unix/serie 5/chapitre 7 apparteee/cx05.8.c
+++ L1/Programmation impérative/Cours/2 - programme c unix/serie 5/chapitre 7 apparteee/cx05.8.c
@@ -1,2 +1,13 @@
+// prototype de la fonction
 void putwords(const char *[]) ;
 int main(int k, const char * argv[]) { putwords(argv) ; return 0 ; }
+
+
+
+/* DEMARCHE
+
+gcc -c cx05.8.c cx05.9.c        // creation des fichier .o
+ar -r libessai.a cx05.9.o       // creation de la library
+gcc -Wall cx05.8.c libessai.a -o essai     // generation de l'executable
+
+*/
